131eef8e7cd3a6101272f556daeacc67aad0da4a
JoV5 <360870202@qq.com>
2017-02-07 10:41:58 +0800

    add eslint、postcss

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -64,35 +64,37 @@ module.exports = {
     publicPath: publicPath
   },
 
-  // TODO resolve
-/*  resolve: {
+  resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
     // We read `NODE_PATH` environment variable in `paths.js` and pass paths here.
     // We use `fallback` instead of `root` because we want `node_modules` to "win"
     // if there any conflicts. This matches Node resolution mechanism.
     // https://github.com/facebookincubator/create-react-app/issues/253
-    fallback: paths.nodePaths,
+    // TODO fallback
+    //fallback: paths.nodePaths,
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
     // https://github.com/facebookincubator/create-react-app/issues/290
-    extensions: ['.js', '.json', '.jsx', ''],
+    extensions: ['.js', '.json', '.jsx'],
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web'
     }
-  },*/
+  },
 
   module: {
     rules: [
-      // TODO eslint
-/*      {
+      // First, run the linter.
+      // It's important to do this before Babel processes the JS.
+      {
         test: /\.(js|jsx)$/,
-        loader: 'eslint',
+        loader: 'eslint-loader',
         include: paths.appSrc,
         enforce: 'pre'
-      },*/
+      },
+
       // Default loader: load all assets that are not handled
       // by other loaders with the url loader.
       // Note: This list needs to be updated with every change of extensions
@@ -121,20 +123,20 @@ module.exports = {
           name: 'static/media[name].[hash:8].[ext]'
         }
       },
+
       // Process JS with Babel.
       {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel-loader',
         options: {
-
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
           cacheDirectory: true
         }
       },
-      // TODO postcss
+
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
       // "style" loader turns CSS into JS modules that inject <style> tags.
@@ -142,12 +144,44 @@ module.exports = {
       // in development "style" loader enables hot editing of CSS.
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader?importLoaders=1'
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ]
+                  }),
+                ];
+              }
+            }
+          }
+        ]
       },
+
+      // JSON is not enabled by default in Webpack but both Node and Browserify
+      // allow it implicitly so we also enable it.
       {
         test: /\.json/,
         loader: 'json-loader'
       },
+
+      // "file" loader for svg
       {
         test: /\.svg$/,
         loader: 'file-loader',
@@ -157,21 +191,6 @@ module.exports = {
       }
     ]
   },
-
-  // TODO postcss
-  // We use PostCSS for autoprefixing only.
-/*  postcss: function() {
-    return [
-      autoprefixer({
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9', // React doesn't support IE8 anyway
-        ]
-      }),
-    ];
-  },*/
 
   plugins: [
     // Makes the public URL available as %PUBLIC_URL% in index.html, e.g.:

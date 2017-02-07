ca4de299c03f55b5b1775f0f4b15eaaf5b59d46e
JoV5 <360870202@qq.com>
2017-02-07 16:44:07 +0800

    add react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './App'
+
+const rootEl = document.getElementById('root');
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <AppContainer>
+    <App/>
+  </AppContainer>,
+  rootEl
 );
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp/>
+      </AppContainer>,
+      rootEl
+    );
+  });
+}
